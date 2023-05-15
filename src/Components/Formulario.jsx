1fd4b388f344ba46b3695e91f75d6a24bac59910
Isabel <isamorenoc08@gmail.com>
2023-05-15 16:47:32 -0500

    Login

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -2,6 +2,12 @@ export function Formulario()[
     return(
         <section>
             <h1>Login</h1>
+
+            <form>
+                <input type="text" />
+                <input type="password" />
+                <button>Iniciar sesion</button>
+            </form>
         </section>
     )
 ]
