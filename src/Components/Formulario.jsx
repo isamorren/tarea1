fc11642b6fe7eaa1746e422a8ff21a1569541994
Isabel <isamorenoc08@gmail.com>
2023-05-15 17:53:23 -0500

    Login

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,13 +1,45 @@
-export function Formulario()[
-    return(
+import "./Formulario.css"
+import { useState } from "react"
+
+export function Formulario() {
+    const [nombre, setNombre] = useState("")
+    const [contraseña, setContraseña] = useState("")
+    const[error, setError] = useState(false)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(nombre == "" || contraseña == "") {
+            setError(true)
+            return
+        }
+
+        setError{false}
+        
+    }
+
+    return (
         <section>
             <h1>Login</h1>
 
-            <form>
-                <input type="text" />
-                <input type="password" />
-                <button>Iniciar sesion</button>
+            <form 
+                className="formulario"
+                onSubmit={handleSubmit}
+            >
+                <input 
+                    type="text" 
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    value={contraseña}
+                    onChange={e => setContraseña(e.target.value)}
+                />
+                <button>Iniciar sesión</button>
             </form>
+            {error && <p>Todos los campos son obligatorios</p>}
         </section>
     )
-]
+}
